9c6e25ceca4020b17441cebc7ef05ec81bfdd3e9
seujeong <wjdzh8748@naver.com>
2021-01-06 22:57:17 +0900

    seujeong

--- get_next_line.h
+++ get_next_line.h
@@ -25,7 +25,7 @@
 #  define OPEN_MAX 32
 # endif
 
-int     get_next_line(int fd, char **line);
+int		get_next_line(int fd, char **line);
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
